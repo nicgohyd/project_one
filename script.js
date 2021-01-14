f5d76f10c9c4792587a68dc84446c3a742539bce
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2021-01-14 20:18:50 +0800

    made gap in wall iterative and random at each iteration.

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //console.log("Test");
 
+var wall = document.getElementById("wall");
+var gap = document.getElementById("hole");
+
+gap.addEventListener('animationinteration', () => {
+    var random = -((Math.random()*300)+150);
+    gap.style.top = random + "px";
+})
